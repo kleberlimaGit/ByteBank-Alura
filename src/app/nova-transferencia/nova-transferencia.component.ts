b7b3cf7709d7cc5d91e500c91bcba6a7d1caf7c2
kleberlimagit <mov_kleber@hotmail.com>
2021-05-27 16:20:48 -0300

    mostrar transferencia na tela

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  //Output propaga os dados para fora
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino:number = 0;
+
   transferir() {
     console.log('Transferindo meu Real');
+    const valorAgTransferencia = {valor:this.valor,destino:this.destino}
+    this.aoTransferir.emit(valorAgTransferencia);
+    this.limparCampos();
   }
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0
+  }
+
 }
+
